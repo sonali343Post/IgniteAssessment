6fcdf0f60ca99c75b5e8f4b710bfc3636cecb213
sonali343Post <sonali.e.panda@accenture.com>
2023-07-30 13:43:50 +0530

    updated test with testng class

--- src/test/java/Automation/UIAutomationTest.java
+++ src/test/java/Automation/UIAutomationTest.java
@@ -1,39 +1,30 @@
 package Automation;
 
 import java.util.concurrent.TimeUnit;
-
-import dev.failsafe.internal.util.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
-
+import org.testng.Assert;
+import org.testng.annotations.Test;
 public class UIAutomationTest {
-    public static void main(String[] args) {
+    @Test
+    public void verifyAppleIphone() {
         try {
             WebDriverManager.edgedriver().setup();
-
             WebDriver driver = new EdgeDriver();
-            //       WebDriver driver = new ChromeDriver();
-
             driver.manage().window().maximize();
             driver.get("https://www.amazon.in/");
             driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
             driver.findElement(By.id("twotabsearchtextbox")).sendKeys("iphone");
             driver.findElement(By.id("nav-search-submit-button")).click();
             Boolean display = driver.findElement(By.xpath("//span[contains(text(),'Apple iPhone')]")).isDisplayed();
-            if (display = true) {
-                System.out.println("First result contains the text 'Apple iPhone'");
-            }
-
+            Assert.assertTrue(display,"First result contains the text 'Apple iPhone'");
             driver.quit();
         }
         catch (Exception e){
 
         }
     }
-
-
 
 }
